9c86a71b75334a4dec0494e7ec85f6b0669abe83
Koki Admin <kokiadmin@koki.id>
2019-09-29 17:05:16 +0700

    first commit

---
+++ src/main/java/org/hakeem/unidata/app/UnidataApplication.java
@@ -0,0 +1,13 @@
+package org.hakeem.unidata.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidataApplication.class, args);
+	}
+
+}
